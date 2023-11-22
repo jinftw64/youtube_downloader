6a176ad4840ae8598fa9fbf4de3cd1b70846d087
Jin Park <103798287+jinftw64@users.noreply.github.com>
2023-11-22 15:53:31 -0500

    fix file path retrieval

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,41 +1,66 @@
 import os
+import sys
 import subprocess
 import shlex
 from pytube import YouTube
 
-print(os.getenv('HOME'))
+def getSavePath():
+    home = os.getenv('HOME')
+    return home + '/Downloads/'
 
-home = os.getenv('HOME')
-save_path = home + '/Downloads/'
+def download():
+    save_path = getSavePath()
+    link = input('Enter URL of YouTube video: ')
 
-link = input('Enter URL of YouTube video: ')
+    # create YouTube object
+    yt = YouTube(link)
 
-yt = YouTube(link)
+    try:
+        # filter by adaptive streams (seperate video and audio tracks)
+        # select the highest quality video
+        video_streams = yt.streams.filter(adaptive=True, file_extension='webm', type='video')
+        video_streams = video_streams.first()
 
-video_streams = yt.streams.filter(adaptive=True, file_extension='webm', type='video')
-video_streams = video_streams.first()
+        # create video name without extension
+        video_name = video_streams.default_filename
+        video_name = video_name.split('.')[0]
 
-video_name = video_streams.default_filename
-video_name = video_name.split('.')[0]
-print(video_name)
+        # download video
+        video_streams.download(output_path=save_path, filename='video.webm')
+    except Exception as e:
+        print(f"Error while downloading video stream: {e}")
+        sys.exit(1)
 
-video_streams.download(output_path=save_path, filename='video.webm')
+    try:
+        # filter by adaptive streams (seperate video and audio tracks)
+        # select the highest quality audio
+        audio_streams = yt.streams.filter(adaptive=True, file_extension='webm', type='audio')
+        audio_streams = audio_streams.first()
 
-audio_streams = yt.streams.filter(adaptive=True, file_extension='webm', type='audio')
-audio_streams = audio_streams.first()
-audio_streams.download(output_path=save_path, filename='audio.webm')
+        # download audio
+        audio_streams.download(output_path=save_path, filename='audio.webm')
+    except Exception as e:
+        print(f"Error while downloading audio stream: {e}")
+        sys.exit(1)
 
-print('Download complete.')
+    print('Success! Downloaded video and audio stream.')
 
-command = ['ffmpeg', '-i', '{}video.webm'.format(save_path), '-i', '{}audio.webm'.format(save_path), '-c', 'copy', '{}'.format(save_path) + '{}.mp4'.format(shlex.quote(video_name)) ]
+def getFilePaths():
+    save_path = getSavePath()
+    video_path = save_path + 'video.webm'
+    audio_path = save_path + 'audio.webm'
+    return video_path, audio_path
 
-try:
-    print('Merging video and audio files...')
-    subprocess.run(command)
-except:
-    pass
+def merge(filepath1, filepath2, destination):
+    command = ['ffmpeg', '-i', '{}video.webm'.format(save_path), '-i', '{}audio.webm'.format(save_path), '-c', 'copy', '{}'.format(save_path) + '{}.mp4'.format(shlex.quote(video_name)) ]
 
-# os.remove(f"{save_path}video.webm")
-# os.remove(f"{save_path}audio.webm")
+    try:
+        print('Merging video and audio files...')
+        subprocess.run(command)
+    except:
+        pass
 
-print('Download completed')
+    # os.remove(f"{save_path}video.webm")
+    # os.remove(f"{save_path}audio.webm")
+
+    print('Download completed')
